fcea9ac3b8586f263fc37b8b51eca24d770c2858
Your Name <you@example.com>
2026-04-02 18:21:26 +0530

    new updated code

--- app/lib/axios.tsx
+++ app/lib/axios.tsx
@@ -1,21 +1,9 @@
 
-
-
-
-
-
 import axios from 'axios';
 
-
 const instance = axios.create({
-    baseURL: "https://api.readymealz.in/api",
+    baseURL: "http://localhost:5000/api",
     withCredentials: true,
 });
 
 export default instance;
-
-
-
-
-
-
